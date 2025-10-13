3eb3272192265501a8c6277ba05f6091fa055001
Pauldarlefelbacq <paul.darle-felbacq@edu.univ-fcomte.fr>
2025-10-13 21:18:22 +0200

    affichage global

--- src/WeatherComponent.jsx
+++ src/WeatherComponent.jsx
@@ -5,6 +5,10 @@ function Weather(){
     const [villelat, setVillelat] = useState(null);
     const [villelong, setVillelong] = useState(null);
     const [ville, setVille] = useState("Paris");
+    const [date, setDate] = useState(null);
+    const [nextHourForecast, setNextHourForecast] = useState(null);
+    
+
 
     useEffect(()=>{
         const fetchvilleweather = async()=>{
@@ -22,24 +26,47 @@ function Weather(){
             setVillelong(long);
 
 
-            const weatherdata = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current_weather=true&hourly=temperature_2m,weathercode,windspeed_10m`)
+            const weatherdata = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current_weather=true&hourly=temperature_2m,apparent_temperature,precipitation,surface_pressure,is_day,wind_speed_10m,precipitation_probability`)
             .then(response => response.json())
             
             setWeatherData(weatherdata);
             console.log("données météto", weatherdata);
-            
+
+            const datee = new Date(weatherdata.current_weather.time);
+            const options = {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+            };
+
+            const formattedDate = new Intl.DateTimeFormat('fr-FR', options).format(datee);
+            setDate(formattedDate);
+            console.log(formattedDate);
+
+            const currentIndex = weatherdata.hourly.time.findIndex(time => time === weatherdata.current_weather.time);
+            if (currentIndex !== -1 && currentIndex + 1 < weatherdata.hourly.time.length) {
+                const nextHourIndex = currentIndex + 1;
+                const forecastTemp = weatherdata.hourly.apparent_temperature[nextHourIndex];
+                const forecastPressure = weatherdata.hourly.surface_pressure[nextHourIndex];
+                setNextHourForecast({
+                    temp: forecastTemp,
+                    pressure:forecastPressure
+                });
+            }
             
         };
         fetchvilleweather();
     },
     []);
+
+
     if (weatherData === null){
         return(
             <div>
                 <h1>Chargement en cours...</h1>
             </div>
         )
-    }
+    };
 
     return (
         <div>
@@ -47,9 +74,38 @@ function Weather(){
             <div>
                 <input type="text" name="search" id="search" />
                 <button>Search</button>
+                
             </div>
             <div>
-                <p>Il fait {weatherData.current_weather.temperature}<strong>{weatherData.current_weather_units.temperature}</strong> à {ville}</p>
+                <p>{ville}</p>
+                <p>{weatherData.current_weather.temperature}</p>
+                <p>{date}</p>
+            </div>
+            <div>
+                {nextHourForecast.temp ? (
+                    <div>
+                        <p>Ressenti</p>
+                        <p>{nextHourForecast.temp}°</p>
+                    </div>
+                ) : (
+                    <div>
+                        <p>Chargement de la prévision...</p>
+                    </div>
+                )}
+                <div>
+                    <h3>Vent</h3>
+                    <p>{weatherData.current_weather.windspeed} km/h</p>
+                </div>
+                {nextHourForecast.pressure ?(
+                    <div>
+                        <h3>Précipitation</h3>
+                        <p>{nextHourForecast.pressure} Bar</p>
+                    </div>
+                ) : (
+                    <div>
+                        <p>Chargement de la prévision...</p>
+                    </div>
+                )}
             </div>
         </div>
     );
